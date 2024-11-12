93f593bd52b0db1e0df0b8051f7ce47b6e894ff4
lilybrongo <144718640+lilybrongo@users.noreply.github.com>
2024-11-12 12:45:33 -0500

    still working code.js

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ function dijkstra(graph, sourceNode) {
   let distance = Array(graph.length).fill(Infinity);
   
   // Array to keep track of visited nodes
-  let visitedNodes = [];
+  let visitedNodes = new Set();
   
   // Set the distance from the source node to itself as 0
   distance[sourceNode] = 0;
@@ -21,7 +21,7 @@ function dijkstra(graph, sourceNode) {
     // Find the unvisited node with the smallest distance
   
     for (let i = 0; i < graph.length; i++) {
-      if (distance[i] < minDistance && !visitedNodes.includes(i)) {
+      if (distance[i] < minDistance && !visitedNodes.has(i)) {
         minDistance = distance[i];
         node = i;
       }
@@ -29,12 +29,12 @@ function dijkstra(graph, sourceNode) {
     // This is required because if no unvisited node was found, I need to be able to break the loop
     if (node === -1) break;
     //needed to modify from push to add in order to work with .has()
-    visitedNodes.push(node);
+    visitedNodes.add(node);
     
     // Update distances to adjacent nodes, and work on calculating nextNode
     // ran into errors on my testing revieved [0,7,8,10,8,5] which was not the expected so added in these tests
     for (let nextNode = 0; nextNode < graph.length; nextNode++) {
-      if (graph[node][nextNode] > 0 && !visitedNodes.includes(nextNode)) {
+      if (graph[node][nextNode] > 0 && !visitedNodes.has(nextNode)) {
         let newDistance = distance[node] + graph[node][nextNode];
          console.log(`Considering edge ${node} -> ${nextNode}, Current distance: ${distance[nextNode]}, New distance: ${newDistance}`);
         
