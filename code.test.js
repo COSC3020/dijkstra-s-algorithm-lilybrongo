2118f12a64c98cf50b7fc6a0559d20ea02240989
lilybrongo <lbrongo@uwyo.edu>
2024-11-29 22:13:53 -0500

    initial commit

--- code.test.js
+++ code.test.js
@@ -7,7 +7,6 @@ eval(fs.readFileSync('code.js')+'');
 
 function runTests() {
   // Test case 1: Simple graph
-  // This test will check if a simple graph with a straightforward path will return the correct distances.
   const graph1 = [
     [0, 1, 4, 0, 0, 0],
     [1, 0, 4, 2, 7, 0],
@@ -19,21 +18,7 @@ function runTests() {
   const result1 = dijkstra(graph1, 0);
   assert.deepStrictEqual(result1, [0, 1, 4, 3, 7, 9]);
 
-  // Test case 2: Larger graph with different weights
-  // This test should make sure that when the graph has various weights, the algorithm should still choose the shortest path.
-  const graph2 = [
-    [0, 10, 0, 0, 0, 5],
-    [10, 0, 1, 0, 0, 2],
-    [0, 1, 0, 4, 0, 0],
-    [0, 0, 4, 0, 2, 8],
-    [0, 0, 0, 2, 0, 3],
-    [5, 2, 0, 8, 3, 0]
-  ];
-  const result2 = dijkstra(graph2, 0);
-  assert.deepStrictEqual(result2, [0, 7, 9, 13, 10, 5]);
-
-  // Test case 3: Disconnected graph
-  // This test will check if unreachable nodes retain their initial distance (infinity). Meaning it will not update the distance of unvisited nodes.
+  // Test case 2: Disconnected graph
   const graph3 = [
     [0, 1, 0],
     [1, 0, 0],
